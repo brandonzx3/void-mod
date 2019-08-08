64e5784f2b271bb9deed0955eeee76859007f1bd
brandon <btherr2003@gmail.com>
2019-08-08 01:06:06 -0500

    lang

--- src/main/java/com/brandonzx3/voidmod/init/ModBlocks.java
+++ src/main/java/com/brandonzx3/voidmod/init/ModBlocks.java
@@ -3,9 +3,12 @@ package com.brandonzx3.voidmod.init;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.brandonzx3.voidmod.blocks.BlockBase;
+import com.brandonzx3.voidmod.blocks.CopperBlock;
 import com.brandonzx3.voidmod.blocks.CopperOre;
 import com.brandonzx3.voidmod.blocks.RubyBlock;
 import com.brandonzx3.voidmod.blocks.RubyOre;
+import com.brandonzx3.voidmod.blocks.TungstenBlock;
 import com.brandonzx3.voidmod.blocks.TungstenOre;
 
 import net.minecraft.block.Block;
@@ -18,4 +21,6 @@ public class ModBlocks{
     public static final Block RUBY_ORE = new RubyOre("ruby_ore", Material.ROCK);
     public static final Block TUNGSTEN_ORE = new TungstenOre("tungsten_ore", Material.ROCK);
     public static final Block COPPER_ORE = new CopperOre("copper_ore", Material.ROCK);
+    public static final Block TUNGSTEN_BLOCK = new TungstenBlock("tungsten_block", Material.IRON);
+    public static final Block COPPER_BLOCK = new CopperBlock("copper_block", Material.IRON);
 }
