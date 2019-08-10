9ed05e05c021d565e168c4fabd3a128e8f185eee
brandon <btherr2003@gmail.com>
2019-08-10 12:42:56 -0500

    awhbdia

--- src/main/java/com/brandonzx3/voidmod/world/ModWorldGen.java
+++ src/main/java/com/brandonzx3/voidmod/world/ModWorldGen.java
@@ -26,7 +26,7 @@ public class ModWorldGen implements IWorldGenerator{
         generateOre(ModBlocks.COPPER_ORE.getDefaultState(), world, random, chunkX * 16, chunkZ * 16, 4, 64, random.nextInt(8) + 3, 15);
         generateOre(ModBlocks.BARITE_ORE.getDefaultState(), world, random, chunkX * 16, chunkZ * 16, 4, 25, random.nextInt(5) + 1, 10);
         generateOre(ModBlocks.PYRITE_ORE.getDefaultState(), world, random, chunkX * 16, chunkZ * 16, 4, 30, random.nextInt(9) + 4, 6);
-        generateOre(ModBlocks.AZURITE_ORE.getDefaultState(), world, random, chunkX * 16, chunkZ * 16, 1, 15, random.nextInt(10) + 1, 4);
+        generateOre(ModBlocks.AZURITE_ORE.getDefaultState(), world, random, chunkX * 16, chunkZ * 16, 1, 25, random.nextInt(10) + 1, 4);
     }
 
     private void generateOre(IBlockState ore, World world, Random random, int x, int z, int minY, int maxY, int size, int chances){
